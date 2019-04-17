2d92eee4b02b33769e1ace0cf6aa401a30845949
Rydus <rydus@ryduss-mac-mini.local>
2019-04-18 01:55:46 +0500

    Initial Commit

---
+++ MovieTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieTest
+//
+//  Created by Rydus on 18/04/2019.
+//  Copyright © 2019 Rydus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
